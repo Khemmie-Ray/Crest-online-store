260b46f1d209babd3e6a65f056f485f89b22419f
Khemmie-Ray <atokemmy@gmail.com>
2023-08-24 12:51:23 +0100

    adding cart functionality

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
-const allProduct = document.querySelector('#catalogue')
+const allProduct = document.querySelector('#catalogue');
 const itemsPerPage = 6;
 let currentPage = 1;
-let allProductsData = []
+let allProductsData = [];
 
 function fetchAllProducts(page) {
     fetch('https://fakestoreapi.com/products', {
         cache: "force-cache"
     })
-    .then(res=>res.json())
+    .then(res => res.json())
     .then(data => {
-        console.log(data)
         allProductsData = data;
+
         fetchAndDisplayProducts(currentPage);     
-    })
+    });
 }
 
 function fetchAndDisplayProducts(page) {
@@ -21,34 +21,48 @@ function fetchAndDisplayProducts(page) {
     const productsToDisplay = allProductsData.slice(startIndex, endIndex);
 
     allProduct.innerHTML = productsToDisplay.map(displayAllProduct).join("");
+
+    const productCards = document.querySelectorAll('.product-card');
+        productCards.forEach(card => {
+            card.addEventListener('click', () => {
+                const productId = card.dataset.id;
+                const productData = allProductsData.find(product => product.id === parseInt(productId));
+                showProduct(productData);
+            });
+    });
 }
 
-    function displayAllProduct(info) {
-        let card = `<div class="mb-8 bg-white p-4 rounded-md lg:w-[32%] w-[100%] lg:h-[450px]">
+function displayAllProduct(info) {
+    let card = `<div class="product-card mb-8 bg-white p-4 rounded-md lg:w-[32%] w-[100%] lg:h-[450px]" data-id=${info.id}>
         <img src=${info.image} alt="" class="object-cover object-top w-[100%] h-[220px] lg:h-[250px] rounded-md"/>
         <p class="font-[700] text-[1.2rem] mt-4">${info.title}</p>
         <p class="text-green-800 font-[700]">$${info.price}</p>
         <a href="" class="text-[2rem] text-black"><i class="ri-shopping-cart-2-fill"></i></a>
-        </div>`
-        return card;
+    </div>`;
+    return card;
+}
+
+const prevButton = document.querySelector('#prev-btn');
+const nextButton = document.querySelector('#next-btn');
+
+prevButton.addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        fetchAndDisplayProducts(currentPage);
     }
-
-    fetchAllProducts();
-
-    const prevButton = document.querySelector('#prev-btn');
-    const nextButton = document.querySelector('#next-btn');
-
-    prevButton.addEventListener('click', () => {
-        if (currentPage > 1) {
-            currentPage--;
-            fetchAndDisplayProducts(currentPage);
-        }
-    });
+});
     
-    nextButton.addEventListener('click', () => {
-        const maxPage = Math.ceil(allProductsData.length / itemsPerPage);
-        if (currentPage < maxPage) {
-            currentPage++;
-            fetchAndDisplayProducts(currentPage);
-        }
-    });
+nextButton.addEventListener('click', () => {
+    const maxPage = Math.ceil(allProductsData.length / itemsPerPage);
+    if (currentPage < maxPage) {
+        currentPage++;
+        fetchAndDisplayProducts(currentPage);
+    }
+});
+
+fetchAllProducts();
+
+const showProduct = (productData) => {
+    localStorage.setItem("productData", JSON.stringify(productData))
+    window.location.href = "product.html";
+}
--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 const featuredCard = document.querySelector('#featured')
 const newArrivals = document.querySelector('#arrivals')
-const cartFeatured = document.querySelector('#cart-featured')
 
-
-fetch("https://fakestoreapi.com/products/category/jewelery")
+fetch("https://fakestoreapi.com/products/category/jewelery", {
+    cache: "force-cache"
+})
     .then(res=>res.json())
     .then(data => {
         
-    featuredCard.innerHTML = data.map(displayProduct).join("") 
+    featuredCard.innerHTML = data.map((datum) => displayProduct(datum)).join("") 
     
     const productCards = document.querySelectorAll('.product-card');
     productCards.forEach(card => {
         card.addEventListener('click', () => {
-            const productId = card.getAttribute('dataId');
+            const productId = card.dataset.id;
             const productData = data.find(product => product.id === parseInt(productId));
             showProduct(productData);
-            console.log(productData);
         });
     });
     })
 
     function displayProduct(info) {
-        let card = `<div class="product-card mb-8 lg:mb-0 bg-white p-4 rounded-md lg:w-[22%] w-[100%] lg:h-[380px]">
-        <img src=${info.image} alt="" class="object-cover object-center w-[100%] h-[200px] lg:h-[150px] rounded-md" dataId=${info.id}/>
+        let card = `<div data-id=${info.id} class="product-card mb-8 lg:mb-0 bg-white p-4 rounded-md lg:w-[22%] w-[100%] lg:h-[380px]">
+        <img src=${info.image} alt="" class="object-cover object-center w-[100%] h-[200px] lg:h-[150px] rounded-md" />
         <p class="font-[700] text-[1.2rem] mt-4">${info.title}</p>
         <p class="text-green-800 font-[700]">$${info.price}</p>
         <a href="" class="text-[2rem] text-black"><i class="ri-shopping-cart-2-fill"></i></a>
@@ -41,16 +40,15 @@ fetch("https://fakestoreapi.com/products/category/jewelery")
         const productCards = document.querySelectorAll('.product-card');
         productCards.forEach(card => {
             card.addEventListener('click', () => {
-                const productId = card.getAttribute('dataId');
+                const productId = card.dataset.id;
                 const productData = data.find(product => product.id === parseInt(productId));
                 showProduct(productData);
-                console.log(productData);
             });
         });
     })
 
     function displayArrivals(info) {
-        let card = `<div class="product-card mb-8 bg-white p-4 rounded-md lg:w-[32%] w-[100%] lg:h-[450px] h-auto" dataId=${info.id}>
+        let card = `<div class="product-card mb-8 bg-white p-4 rounded-md lg:w-[32%] w-[100%] lg:h-[450px] h-auto" data-id=${info.id}>
         <img src=${info.image} alt="" class="object-cover object-top w-[100%] h-[220px] lg:h-[250px] rounded-md"/>
         <p class="font-[700] text-[1.2rem] mt-4">${info.title}</p>
         <p class="text-green-800 font-[700]">$${info.price}</p>
